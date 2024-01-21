934f9d2fb712ff890f0817b1717afeb7e2fbd706
Alexander Chneerov <alexanderchneerov@alexandrslaptop.wireless.concordia.ca>
2024-01-21 02:24:24 -0500

    the commit to end all commits, we added some features

--- controllers/rideController.js
+++ controllers/rideController.js
@@ -2,6 +2,9 @@ const config = require('../config.js');
 const db = require('../database/database.js');
 const asyncHandler = require('express-async-handler');
 
+
+const polyline = require('polyline');
+
 // Display new ride page.
 exports.new_ride = asyncHandler(async (req, res, next) => {
     res.render('new_ride', { key: config.directionsAPI });
@@ -9,18 +12,108 @@ exports.new_ride = asyncHandler(async (req, res, next) => {
 
 // API call for new ride.
 exports.get_directions = asyncHandler(async (req, res, next) => {
-    // const client = new Client({});
+    function findIndexOfMin(arr) {
+        if (!arr.length) {
+            return null;
+        }
+    
+        const minValue = Math.min(...arr);
+        const minIndex = arr.indexOf(minValue);
+    
+        return minIndex;
+    }
+    
+    function apiToArr(folderName) {
+        const allCoordinatesArr = [];
+        const files = fs.readdirSync(folderName);
+    
+        for (const routeFile of files) {
+            const coordinatesArray = [];
+            const data = JSON.parse(fs.readFileSync(`${folderName}/${routeFile}`, 'utf8'));
+    
+            for (const route of data.routes) {
+                for (const leg of route.legs) {
+                    for (const step of leg.steps) {
+                        const polylineLine = step.polyline.points;
+                        const decodedCoordinates = polyline.decode(polylineLine);
+                        for (const [lat, lng] of decodedCoordinates) {
+                            coordinatesArray.push([lat, lng]);
+                        }
+                    }
+                }
+            }
+    
+            allCoordinatesArr.push(coordinatesArray);
+        }
+        return allCoordinatesArr;
+    }
+    
+    const fs = require('fs');
 
-    // const response = await client.directions({
-    //     params: {
-    //         origin: 'Chicago',
-    //         destination: 'Los Angeles',
-    //         key: config.directionsAPI,
-    //     },
-    //     timeout: 1000, // milliseconds
-    // });
-    // console.log(response.data);
+    console.log("Swag");
+    
+    function readCollisionsCsvAndCreateArray(csvFile) {
+        const dataArray = [];
+        db.serialize(() => {
+            db.serialize(() => {
+                db.all(`SELECT * FROM collisions LIMIT 1`, (err, rows) => {
+                   if (err) {
+                     console.error(err.message);
+                   }
+                   rows.forEach((row) => {
+                    dataArray.push([ row.LOC_LONG, row.LOC_LAT ]);
+                   })
+                });
+               });
+        });
+        return dataArray;
+    }
+    
+    
+    
+    function closeToCollision(coordPair, collisionDataArr) {
+        const [longA, longB] = coordPair;
+    
+        for (const collCoordPair of collisionDataArr) {
+            if (Math.abs(longA - collCoordPair[0]) < 0 || Math.abs(longB - collCoordPair[1]) < 0.0001) {
+                return true;
+            }
+        }
+    
+        return false;
+    }
+    
+    function findSafestRoute(allRouteDataArr, collisionDataArr) {
+        const collisionScoreArr = [];
+    
+        for (let i = 0; i < allRouteDataArr.length; i++) {
+            let collisionCounter = 0;
+            for (const coordPair of allRouteDataArr[i]) {
+                if (closeToCollision(coordPair, collisionDataArr)) {
+                    collisionCounter++;
+                }
+            }
+            collisionScoreArr.push(collisionCounter);
+        }
+    
+        return findIndexOfMin(collisionScoreArr);
+    }
+    
+    try {
+        console.log("1")
+        const allRouteDataArr = apiToArr('routes_txt_folder');
+        console.log("2")
+        const collisionCsvFileName = 'collisions.csv';
+        const collisionDataArr = readCollisionsCsvAndCreateArray(collisionCsvFileName);
+        const safestRouteIndex = findSafestRoute(allRouteDataArr, collisionDataArr);
+        console.log(safestRouteIndex);
+    } catch (error) {
+        console.log(error);
+    }
+    
+
 
     res.json({ message: 'Hello, World!' });
     // send respone back to client...
 });
+
