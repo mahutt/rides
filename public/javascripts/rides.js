abad15c3a06d5a6f5564efed80982618519eea10
vinishamanek <117116432+vinishamanek@users.noreply.github.com>
2024-01-20 22:42:17 -0500

    Geocoder added

--- public/javascripts/rides.js
+++ public/javascripts/rides.js
@@ -11,24 +11,102 @@ function initMap() {
     directionsRenderer.setMap(map);
 
     document.getElementById('ride').addEventListener('click', async () => {
+        
+    function geocodeAddress(address, callback) {
+            const geocoder = new google.maps.Geocoder();
+            geocoder.geocode({ address: address }, (results, status) => {
+                if (status === 'OK') {
+                    const location = results[0].geometry.location;
+                    const latLng = { lat: location.lat(), lng: location.lng() };
+                    callback({ address, lat: location.lat(), lng: location.lng() });
+                } else {
+                    console.log('error');
+                }
+            });
+        }
+
+        var start = document.getElementById('start').value;
+        var end = document.getElementById('end').value;
+
+
+        geocodeAddress(start, (startLocation) => {
+            geocodeAddress(end, (endLocation) => {
+                // const start = startLocation.latLng;
+                // const end = endLocation.latLng;
+                // console.log(start);
+                // console.log(end);
+
+                // midvector = ((end - start)/2);
+                // console.log(midvector);
+
+                // midpoint = ((start) + midvector);
+                // console.log(midpoint);
+
+                // // extra1 = midpoint + ()
+
+                const startLat = startLocation.lat;
+                const startLng = startLocation.lng;
+                const endLat = endLocation.lat;
+                const endLng = endLocation.lng;
+            
+                console.log('start lat:', startLat);
+                console.log('start lng:', startLng);
+                console.log('end lat:', endLat);
+                console.log('end lng:', endLng);
+
+                const midpointvectorLat = (endLat - startLat)/2;
+                console.log('midpointvectorLat:', midpointvectorLat);
+
+                const midpointvectorLng = (endLng - startLng)/2;
+                console.log('midpointvectorLng:', midpointvectorLng);
+
+                const midpointLat = (startLat + midpointvectorLat);
+                console.log('midpointLat:', midpointLat);
+
+                const midpointLng = (startLng + midpointvectorLng);
+                console.log('midpointLng:', midpointLng);
+
+                const c1Lat = midpointLat - midpointvectorLat;
+                const c1Lng = midpointLng + midpointvectorLng;
+                const c2Lat = midpointLat + midpointvectorLat;
+                const c2Lng = midpointLng - midpointvectorLng;
+                console.log('c1Lat:', c1Lat);
+                console.log('c1Lng:', c1Lng);
+                console.log('c2Lat:', c2Lat);
+                console.log('c2Lng:', c2Lng);
+
+            directionsService.route(
+                {
+                origin: start,
+                destination: end,
+                travelMode: 'DRIVING',
+                },
+                (response, status) => {
+                if (status === 'OK') {
+                    directionsRenderer.setDirections(response);
+                } else {
+                    console.log('Error: ', status);
+                  }
+                }
+            );
+            });
+        });
+        
         function drawRoute(start, end) {
             var request = {
                 origin: start,
                 destination: end,
                 travelMode: 'BICYCLING',
             };
-
-            directionsService.route(request, function (result, status) {
-                if (status == 'OK') {
-                    directionsRenderer.setDirections(result);
-                } else {
-                    console.log('Error: ', status);
-                }
-            });
         }
 
-        var start = 'Quebec City, Canada';
-        var end = 'Montreal, Canada';
+
+        //get coordinates from input using geocoder for node express
+
+        // var start = document.getElementById('start').value;
+        // var end = document.getElementById('end').value;
+        // var start = '1535 rue St Jacques, Montreal, Canada';  // from textbox
+        // var end = '2125 rue Crescent, Montreal, Canada';  // from textbox
         drawRoute(start, end);
     });
 }
